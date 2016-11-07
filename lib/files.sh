d788bf2400936b2efd4015877154e8e5221c98a5
Alberto Franzin <alberto.franzin@gmail.com>
2016-11-07 16:20:36 +0100

    remove empty lines

--- lib/files.sh
+++ lib/files.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# remove empty lines from file
+# remove_empty_lines inputFile outputFile
+remove_empty_lines () { 
+    sed '/^[[:space:]]*$/ d' < $1 > $2
+}
+
 # remove comment lines from file
 # remove_comments inputFile outputFile
 remove_comments () {
-    sed '/^#/ d' < $1 > $2
+    TMP=`mktemp`
+    sed '/^#/ d' < $1 > $TMP
+    remove_empty_lines $TMP $2
+    rm -f $TMP
 }
+
